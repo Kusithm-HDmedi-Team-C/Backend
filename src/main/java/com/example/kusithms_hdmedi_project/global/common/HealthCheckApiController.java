d0deec92457d4b42720e92916fe6be6b816c3bed
OJOJIN <ojy101039@gmail.com>
2023-09-03 17:42:51 +0900

    [feat] #1 healthCheckApi 작성

---
+++ src/main/java/com/example/kusithms_hdmedi_project/global/common/HealthCheckApiController.java
@@ -0,0 +1,12 @@
+package com.example.kusithms_hdmedi_project.global.common;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckApiController {
+    @RequestMapping("/")
+    public String hdMedi() {
+        return "hdMedi-Team-C";
+    }
+}
