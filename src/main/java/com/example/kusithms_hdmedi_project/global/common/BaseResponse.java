9e82992678804aa48f8366cccc5a30aba5887f33
OJOJIN <ojy101039@gmail.com>
2023-09-03 17:43:22 +0900

    [feat] #1 BaseResponse 작성

---
+++ src/main/java/com/example/kusithms_hdmedi_project/global/common/BaseResponse.java
@@ -0,0 +1,26 @@
+package com.example.kusithms_hdmedi_project.global.common;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class BaseResponse<T> {
+    private int status;
+    private String message;
+    private T data;
+
+    @Builder
+    public BaseResponse(int status, String message, T data) {
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> BaseResponse<?> of(SuccessCode successCode, T data) {
+        return BaseResponse.builder()
+                .status(successCode.getHttpStatus().value())
+                .message(successCode.getMessage())
+                .data(data)
+                .build();
+    }
+}
