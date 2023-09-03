fd7d64273731e0385509ac31bb4899c86926d15d
OJOJIN <ojy101039@gmail.com>
2023-09-03 15:12:33 +0900

    init

---
+++ src/main/java/com/example/kusithms_hdmedi_project/KusithmsHdmediProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.kusithms_hdmedi_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusithmsHdmediProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KusithmsHdmediProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kusithms_hdmedi_project/KusithmsHdmediProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kusithms_hdmedi_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusithmsHdmediProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
