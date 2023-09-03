2e15480931834d681893adc123279974ff9fb97d
OJOJIN <ojy101039@gmail.com>
2023-09-03 17:42:08 +0900

    [feat] #1 JpaConfig 작성

---
+++ src/main/java/com/example/kusithms_hdmedi_project/global/config/JpaConfig.java
@@ -0,0 +1,7 @@
+package com.example.kusithms_hdmedi_project.global.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+public class JpaConfig {
+}
