5cb716230d9f407bdec83fe877fff1b5f5f14bbf
yeop0740 <ssucgoo@gmail.com>
2023-09-08 02:29:30 +0900

    [feat] #13 SurveyQuestionRepository 전체 SurveyQuestion 조회 기능 구현

--- src/main/java/com/example/kusithms_hdmedi_project/question/repository/SurveyQuestionRepository.java
+++ src/main/java/com/example/kusithms_hdmedi_project/question/repository/SurveyQuestionRepository.java
@@ -1,2 +1,10 @@
-package com.example.kusithms_hdmedi_project.question.repository;public interface SurveyQuestionRepository {
+package com.example.kusithms_hdmedi_project.question.repository;
+
+import com.example.kusithms_hdmedi_project.question.entity.SurveyQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SurveyQuestionRepository extends JpaRepository<SurveyQuestion, Long> {
+    List<SurveyQuestion> findAll();
 }
