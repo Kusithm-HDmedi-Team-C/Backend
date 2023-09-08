07ebe84726f880d2b06ce2bf00db3d0b44248961
OJOJIN <ojy101039@gmail.com>
2023-09-08 15:00:53 +0900

    [feat] #16 SurveyResultDto 작성

---
+++ src/main/java/com/example/kusithms_hdmedi_project/question/dto/Request/SurveyResultRequestDto.java
@@ -0,0 +1,9 @@
+package com.example.kusithms_hdmedi_project.question.dto.Request;
+
+import lombok.Getter;
+
+@Getter
+public class SurveyResultRequestDto {
+    private Long surveyId;
+    private int score;
+}
