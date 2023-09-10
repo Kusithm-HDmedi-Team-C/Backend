d61cf402107bea3e731b6a93edab2a995f8bd604
OJOJIN <ojy101039@gmail.com>
2023-09-11 03:25:24 +0900

    [feat] #24 ExaminationType of 메소드 추가

--- src/main/java/com/example/kusithms_hdmedi_project/domain/review/entity/ExaminationType.java
+++ src/main/java/com/example/kusithms_hdmedi_project/domain/review/entity/ExaminationType.java
@@ -1,5 +1,9 @@
 package com.example.kusithms_hdmedi_project.domain.review.entity;
 
 public enum ExaminationType {
-    CONSULTATION, CAT, AAA, BBB, CCC, ETC
+    CONSULTATION, CAT, AAA, BBB, CCC, ETC;
+
+    public static ExaminationType of(ReviewExamination reviewExamination){
+        return reviewExamination.getReviewType();
+    }
 }
