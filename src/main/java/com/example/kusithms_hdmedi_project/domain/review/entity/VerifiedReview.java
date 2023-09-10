5c5229914f5b329d95310b4d682aaab6e997f467
OJOJIN <ojy101039@gmail.com>
2023-09-10 18:53:42 +0900

    [feat] #21 리뷰 entity 생성

---
+++ src/main/java/com/example/kusithms_hdmedi_project/domain/review/entity/VerifiedReview.java
@@ -0,0 +1,46 @@
+package com.example.kusithms_hdmedi_project.domain.review.entity;
+
+
+import com.example.kusithms_hdmedi_project.domain.hospital.entity.Hospital;
+import com.example.kusithms_hdmedi_project.global.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "verified_review")
+public class VerifiedReview extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "verified_review_id")
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @Column(name = "docker_name")
+    private String doctor;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @OneToMany(mappedBy = "verifiedReview", fetch = FetchType.LAZY)
+    private List<ReviewExamination> reviewExaminations= new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+}
