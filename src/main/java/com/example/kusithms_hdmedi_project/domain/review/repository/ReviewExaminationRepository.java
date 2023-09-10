0146da051f6e351823bea9aedbbd1acbed4475b1
OJOJIN <ojy101039@gmail.com>
2023-09-11 03:25:41 +0900

    [feat] #24 ReviewExaminationRepository 추가

---
+++ src/main/java/com/example/kusithms_hdmedi_project/domain/review/repository/ReviewExaminationRepository.java
@@ -0,0 +1,7 @@
+package com.example.kusithms_hdmedi_project.domain.review.repository;
+
+import com.example.kusithms_hdmedi_project.domain.review.entity.ReviewExamination;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewExaminationRepository extends JpaRepository<ReviewExamination, Long> {
+}
