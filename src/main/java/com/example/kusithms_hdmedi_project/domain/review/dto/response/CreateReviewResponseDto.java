c02e2f0851ef15656ab31684703d9782018de2cb
OJOJIN <ojy101039@gmail.com>
2023-09-11 03:22:38 +0900

    [feat] #24 리뷰 생성 api response Dto 작성

---
+++ src/main/java/com/example/kusithms_hdmedi_project/domain/review/dto/response/CreateReviewResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.kusithms_hdmedi_project.domain.review.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+public class CreateReviewResponseDto {
+
+    private Long reviewId;
+
+    public static CreateReviewResponseDto of(Long reviewId){
+        return CreateReviewResponseDto.builder()
+                .reviewId(reviewId)
+                .build();
+    }
+}
