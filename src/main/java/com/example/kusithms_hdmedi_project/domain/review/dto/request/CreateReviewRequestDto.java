7d4e4b3e05f8e30d980a28763b68d8bbd1d29719
OJOJIN <ojy101039@gmail.com>
2023-09-11 17:15:41 +0900

    [fix] #24 Response Dto들 사소한.. 수정

--- src/main/java/com/example/kusithms_hdmedi_project/domain/review/dto/request/CreateReviewRequestDto.java
+++ src/main/java/com/example/kusithms_hdmedi_project/domain/review/dto/request/CreateReviewRequestDto.java
@@ -1,25 +1,47 @@
 package com.example.kusithms_hdmedi_project.domain.review.dto.request;
 
+import com.example.kusithms_hdmedi_project.domain.review.dto.response.CreateReviewResponseDto;
 import com.example.kusithms_hdmedi_project.domain.review.entity.ExaminationType;
 import com.example.kusithms_hdmedi_project.domain.review.entity.Review;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
+
+@NoArgsConstructor
 @Getter
 public class CreateReviewRequestDto {
 
     private Long hospitalId;
 
-    private Integer rating;
+    private int rating;
 
     private String content;
 
     private String doctor;
 
-    private Integer price;
+    private int price;
 
     private List<ExaminationType> examinations;
+
+
+    @Builder
+    public static CreateReviewRequestDto of(Long hospitalId,
+                                            int rating,
+                                            String content,
+                                            String doctor,
+                                            int price,
+                                            List<ExaminationType> examinations) {
+        return CreateReviewRequestDto.builder()
+                .hospitalId(hospitalId)
+                .rating(rating)
+                .content(content)
+                .doctor(doctor)
+                .price(price)
+                .examinations(examinations)
+                .build();
+    }
 
 }
--- src/main/java/com/example/kusithms_hdmedi_project/domain/review/dto/response/VerifyReviewResponseDto.java
+++ src/main/java/com/example/kusithms_hdmedi_project/domain/review/dto/response/VerifyReviewResponseDto.java
@@ -1,8 +1,10 @@
 package com.example.kusithms_hdmedi_project.domain.review.dto.response;
 
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class VerifyReviewResponseDto {
 
     private Long verifiedReviewId;
