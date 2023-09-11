f391dcab0d2f36cce580f17711d02828af5acac6
OJOJIN <ojy101039@gmail.com>
2023-09-11 19:05:06 +0900

    [fix] #24 RestController 어노테이션 변경

--- src/main/java/com/example/kusithms_hdmedi_project/domain/review/controller/ReviewController.java
+++ src/main/java/com/example/kusithms_hdmedi_project/domain/review/controller/ReviewController.java
@@ -11,7 +11,6 @@ import com.example.kusithms_hdmedi_project.global.common.SuccessCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,7 +20,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/review")
-@Controller
+@RestController
 public class ReviewController {
 
     private final ReviewService reviewService;
