a5a3f50afe5a658d3a26cbb1a03e95693a4c3827
OJOJIN <ojy101039@gmail.com>
2023-09-13 16:21:14 +0900

    [feat] #36 최신순 병원별 리뷰 받아오는 api 추가

--- src/main/java/com/example/kusithms_hdmedi_project/domain/review/repository/VerifiedReviewRepository.java
+++ src/main/java/com/example/kusithms_hdmedi_project/domain/review/repository/VerifiedReviewRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface VerifiedReviewRepository extends JpaRepository<VerifiedReview, Long> {
 
     Page<VerifiedReview> findByHospitalIdOrderByCreateDateDesc(Long hospitalId, Pageable pageable);
+
+    List<VerifiedReview> findByHospitalIdOrderByCreateDateDesc(Long hospitalId);
 }
