93201eb2ba1b3073e13d30aed5df6aa67a2cba38
yeop0740 <ssucgoo@gmail.com>
2023-09-13 19:31:05 +0900

    [fix] #31 API 스펙 변경에 따른 DTO 수정

--- src/main/java/com/example/kusithms_hdmedi_project/domain/hospital/dto/response/HospitalDetailsDto.java
+++ src/main/java/com/example/kusithms_hdmedi_project/domain/hospital/dto/response/HospitalDetailsDto.java
@@ -1,20 +1,43 @@
 package com.example.kusithms_hdmedi_project.domain.hospital.dto.response;
 
+import com.example.kusithms_hdmedi_project.domain.hospital.entity.Hospital;
+import com.example.kusithms_hdmedi_project.domain.review.dto.response.GetHospitalVReviewResponseDto;
 import com.example.kusithms_hdmedi_project.domain.review.entity.VerifiedReview;
+import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Builder
 @Data
 public class HospitalDetailsDto {
+    private Long hospitalId;
     private String name;
     private double averageRating;
     private String address;
     private String url;
     private String mapUrl;
-    // TODO VerifiedReview -> VerifiedReviewDto 변경 필요
-    private List<VerifiedReview> reviews;
+    private List<GetHospitalVReviewResponseDto> reviews;
     private int pageNumber;
     private int count;
     private boolean hasNextPage;
+
+    public static HospitalDetailsDto of(Hospital hospital, Page<VerifiedReview> page) {
+        return HospitalDetailsDto.builder()
+                .hospitalId(hospital.getId())
+                .name(hospital.getName())
+                .averageRating(hospital.getAverageRating())
+                .address(hospital.getAddress())
+                .url(hospital.getUrl())
+                .mapUrl(hospital.getMapUrl())
+                .reviews(page.getContent().stream()
+                        .map(GetHospitalVReviewResponseDto::of)
+                        .collect(Collectors.toList()))
+                .pageNumber(page.getNumber())
+                .count(page.getNumberOfElements())
+                .hasNextPage(page.hasNext())
+                .build();
+    }
 }
